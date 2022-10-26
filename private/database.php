fe5f8e624a3422977dff2cf3bff151149d3de5aa
Masoud Moniri <masoudmoniri10@gmail.com>
2022-10-26 21:46:48 +1100

    testing

--- private/database.php
+++ private/database.php
@@ -8,15 +8,8 @@ function connect_to_db()
   echo ('<br>2con');
   $connect_error = confirm_db_connect();
   echo ('<br>3con');
-  exit;
 
   if ($connect_error) {
-    $connection = mysqli_connect(DB_HOST2, DB_USER2, DB_PASSWORD2, DB_NAME2);
-    $connect_error = confirm_db_connect();
-  }
-  if ($connect_error) {
-    echo ("second error");
-    var_dump($connect_error);
     exit($connect_error);
   }
   return $connection;
