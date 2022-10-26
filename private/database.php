30363b193b57b3d49d9ab046303919fc8adb9fea
Masoud Moniri <masoudmoniri10@gmail.com>
2022-10-26 21:43:13 +1100

    testing

--- private/database.php
+++ private/database.php
@@ -3,10 +3,13 @@ require_once('db_credentials.php');
 
 function connect_to_db()
 {
-  echo ('1');
-  exit;
+  echo ('1con');
   $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+  echo ('<br>2con');
   $connect_error = confirm_db_connect();
+  echo ('<br>2con');
+  exit;
+
   if ($connect_error) {
     $connection = mysqli_connect(DB_HOST2, DB_USER2, DB_PASSWORD2, DB_NAME2);
     $connect_error = confirm_db_connect();
