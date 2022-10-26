a4445dd5dfccd0d9d418bbf1e151aeb8cc929af2
Masoud Moniri <masoudmoniri10@gmail.com>
2022-10-26 22:29:56 +1100

    testing

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ echo ('$_SERVER["SCRIPT_NAME"]=' . $_SERVER['SCRIPT_NAME']);
 echo "<br>";
 echo ("created=" . $_SERVER['DOCUMENT_ROOT'] . '/private/initialize.php');
 echo "<br>";
+echo ('$_SERVER["SERVER_NAME"]=' . $_SERVER['SERVER_NAME']);
+echo "<br>";
+exit();
 
 //$pos = strpos($_SERVER['SCRIPT_NAME'], 'index');
 //echo (substr($_SERVER['SCRIPT_NAME'], 0, $pos));
