11d55e7b6a13cfd03b5c178678ee5b3b507875b1
Masoud Moniri <masoudmoniri10@gmail.com>
2022-10-26 11:54:57 +1100

    testing

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require_once('./private/initialize.php');
-redirect_to('./public/staff/index.php');
+require_once('/private/initialize.php');
+redirect_to('/public/staff/index.php');
 ?>
 <!--  -->
 <!DOCTYPE html>
